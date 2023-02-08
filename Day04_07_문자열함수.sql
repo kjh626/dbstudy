7034d5a0a0431e9494a1458956bb3c1563d3dc05
kjh626 <wjd1113@naver.com>
2023-02-08 09:29:55 +0900

    2023-02-08

--- Day04_07_문자열함수.sql
+++ Day04_07_문자열함수.sql
@@ -31,3 +31,53 @@ SELECT
      , FIRST_NAME || ' ' || LAST_NAME                     -- JAVA의 +역할을 한다.  
   FROM 
        EMPLOYEES;
+  
+       
+-- 4. 문자열 일부 반환하기
+--    SUBSTR(칼럼, BEGIN, LENGTH) : BEGIN부터 LENGTH개를 반환
+--    주의! BEGIN은 1에서 시작한다.(INDEX가 아니다)
+SELECT 
+       SUBSTR(EMAIL, 1, 3)    -- 1번째 글자부터 3글자를 가져오시오.
+  FROM 
+       EMPLOYEES;
+
+
+-- 5. 특정 문자열의 위치 반환하기
+--    INSTR(칼럼, 찾을문자열)
+--    주의! 반환되는 위치 정보는 인덱스가 아니므로 0부터 시작하지 않고, 1부터 시작한다.
+--    못 찾으면 0을 반환한다.
+SELECT
+       INSTR(EMAIL, 'A')    -- 'A'의 위치를 반환
+  FROM 
+       EMPLOYEES;
+       
+       
+-- 6. 문자열 채우기(PADDING)
+--    1) LPAD(칼럼, 전체폭, 채울문자)
+--    2) RPAD(칼럼, 전체폭, 채울문자)
+SELECT
+       LPAD(DEPARTMENT_ID, 3, '0')
+     , RPAD(EMAIL, 10, '*')
+  FROM
+       EMPLOYEES;
+
+
+-- 연습문제
+SELECT
+       LPAD(NVL(DEPARTMENT_ID, 0), 3, '0')
+     , RPAD(SUBSTR(EMAIL, 1, 2), 5, '*')
+  FROM
+       EMPLOYEES;
+       
+       
+-- 7. 불필요한 공백 제거
+--    1) LTRIM(칼럼) : 왼쪽 공백 제거
+--    2) RTRIM(칼럼) : 오른쪽 공백 제거
+--    3) TRIM(칼럼)  : 왼쪽, 오른쪽 공백 모두 제거
+-- DB의 자료가 엉망일 때 정리하려고, 엑셀같은 거 DB에 임포트할 때 공백붙어서 올 수가 있다. 그래서 TRIM써서 가공할 필요가 있다.
+SELECT
+       '[' || LTRIM('      HELLO') || ']'
+     , '[' || RTRIM('HELLO      ') || ']'
+     , '[' || TRIM('   HELLO    ') || ']'
+  FROM 
+       DUAL;
